5c0e75bd27025bf5d17f5d76c3de07735fff6669
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-14 13:10:48 +0000

    skeleton codebase for achievement tracker application using Spring Initializr

---
+++ src/main/java/com/example/achievement_tracker/AchievementTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.achievement_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievementTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchievementTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/achievement_tracker/AchievementTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achievement_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievementTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
