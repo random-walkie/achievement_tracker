4fa0d86ec574693998e61da855f0b629a208bdd1
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-17 15:55:33 +0000

    Added Spotless gradle plugin for code formatting.
    Added Spotless gradle plugin for code formatting.  Added LICENSE file. Updated license headers, with gradlew + spotlessApply.

--- src/test/java/com/example/achievement_tracker/model/AchievementsTest.java
+++ src/test/java/com/example/achievement_tracker/model/AchievementsTest.java
@@ -1,25 +1,39 @@
+/*
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.achievement_tracker.model;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-
 class AchievementsTest {
 
-  private Achievements achievements;
+    private Achievements achievements;
 
-  @BeforeEach
-  void setUp() {
-    achievements = new Achievements();
-  }
+    @BeforeEach
+    void setUp() {
+        achievements = new Achievements();
+    }
 
-  @Test
-  @DisplayName("Correct Instantiation of an Achievements object.")
-  void correctInstantiationOfAchievementsObject() {
+    @Test
+    @DisplayName("Correct Instantiation of an Achievements object.")
+    void correctInstantiationOfAchievementsObject() {
 
-    assertInstanceOf(Achievements.class, achievements);
-  }
-
+        assertInstanceOf(Achievements.class, achievements);
+    }
 }
