52ec055510944b61ab1e32f16edf92d1190a7c3c
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-22 13:50:22 +0000

    Removed TestEntityManager as no need  for fine-grained control over persistence operations or to simulate specific JPA behaviors.

--- src/test/java/com/example/achievement_tracker/repository/AchievementRepositoryTest.java
+++ src/test/java/com/example/achievement_tracker/repository/AchievementRepositoryTest.java
@@ -19,12 +19,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.achievement_tracker.model.Achievement;
 import com.example.achievement_tracker.model.StatusEnum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.NoSuchElementException;
@@ -34,25 +35,54 @@ import java.util.Optional;
 @DataJpaTest
 public class AchievementRepositoryTest {
     @Autowired private AchievementRepository achievementRepository;
-    @Autowired private TestEntityManager entityManager;
+    private Achievement achievement;
+    private final String title = "Test Achievement";
+    private final StatusEnum status = StatusEnum.COMPLETED;
+
+    @BeforeEach
+    void setUp(){
+        achievement = Achievement.builder().status(status).title(title).build();
+
+        achievementRepository.save(achievement);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Release test data after each test method
+        achievementRepository.delete(achievement);
+    }
 
     @Test
     @DisplayName("Given an achievement, when the achievement is saved, the achievement can be found by title.")
-    void FindByTitle() {
-
-        String title = "Test Achievement";
-        Achievement achievement =
-                Achievement.builder()
-                        .status(StatusEnum.COMPLETED)
-                        .title(title)
-                        .build();
-
-        entityManager.persist(achievement);
-        entityManager.flush();
-
+    void findByTitle() {
+        // The repository returns an Optional to safely handle the case where no achievement is found.
+        // This prevents returning null and avoids potential NullPointerException,
+        // enforcing us to explicitly handle the missing value scenario.
         Optional<Achievement> optionalAchievement = achievementRepository.findByTitle(title);
         Achievement foundAchievement = optionalAchievement.orElseThrow(() ->
                 new NoSuchElementException("Achievement with title '" + title + "' not found"));
+
         assertEquals(title, foundAchievement.getTitle());
     }
+    @Test
+    @DisplayName("Given an achievement, when the achievement is saved, the achievement can be found by status.")
+    void findByStatus() {
+        Optional<Achievement> optionalAchievement = achievementRepository.findByStatus(status);
+        Achievement foundAchievement = optionalAchievement.orElseThrow(() ->
+                new NoSuchElementException("Achievement with status '" + status + "' not found"));
+
+        assertEquals(status, foundAchievement.getStatus());
+    }
+
+    @Test
+    @DisplayName("Given an achievement, when the achievement is saved, the achievement can be found by id.")
+    void findById() {
+        Optional<Achievement> optionalAchievement = achievementRepository.findById(1L);
+        Achievement foundAchievement = optionalAchievement.orElseThrow(() ->
+                new NoSuchElementException("Achievement with id '1' not found"));
+        System.out.println(foundAchievement.getId());
+        assertEquals(1L, foundAchievement.getId());
+
+    }
+
 }
