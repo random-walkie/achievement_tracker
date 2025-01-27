04845332d76015825a12fba5993b737dc9d0a65b
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-27 16:42:41 +0000

    refactored folders

--- src/test/java/com/example/achievement_tracker/persistence/repository/AchievementRepositoryTest.java
+++ src/test/java/com/example/achievement_tracker/persistence/repository/AchievementRepositoryTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.achievement_tracker.repository;
+package com.example.achievement_tracker.persistence.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
