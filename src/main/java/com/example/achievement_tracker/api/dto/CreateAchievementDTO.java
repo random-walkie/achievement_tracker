308ef109bdeea889c008d2cdfed06614d2e436b2
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-31 12:51:35 +0000

    add lombok @Builder.

--- src/main/java/com/example/achievement_tracker/api/dto/CreateAchievementDTO.java
+++ src/main/java/com/example/achievement_tracker/api/dto/CreateAchievementDTO.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record CreateAchievementDTO(
     @NotBlank @Size(max = 50) String title,
     String description,
