2fede9d9bdb1c0021e260e052e494149fbb7ec25
random-walkie <26143642+random-walkie@users.noreply.github.com>
2025-01-31 13:26:30 +0000

    columns should be nullable if a field is not required.

--- src/main/java/com/example/achievement_tracker/persistence/model/Achievement.java
+++ src/main/java/com/example/achievement_tracker/persistence/model/Achievement.java
@@ -53,19 +53,13 @@ public class Achievement {
     @Column(nullable = false, unique = true)
     @NotEmpty(message = "Title cannot be empty.") @Length(max = 50, message = "Title length exceeds the allowed limit.") @NonNull private String title;
 
-    @Column(nullable = false)
-    @Builder.Default
-    private String description = "This is a generic achievement.";
+  @Column @Builder.Default private String description = "This is a generic achievement.";
 
-    @Column(nullable = false)
-    @Builder.Default
-    private LocalDate dateStarted = LocalDate.now();
+  @Column @Builder.Default private LocalDate dateStarted = LocalDate.now();
 
-    @Column(nullable = false)
-    @Builder.Default
-    private LocalDate dateCompleted = LocalDate.now();
+  @Column @Builder.Default private LocalDate dateCompleted = LocalDate.now();
 
-    @ElementCollection @Builder.Default private List<String> tags = new ArrayList<>();
+  @Column @ElementCollection @Builder.Default private List<String> tags = new ArrayList<>();
 
     // required field
     @Enumerated(EnumType.STRING)
